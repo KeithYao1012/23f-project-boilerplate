3ab3c42b7334d60de1c4c15fd99200c0c342a9be
Maithili Ubgade <ubgade.m@northeastern.edu>
2024-04-12 19:39:41 -0400

    Update __init__.py
    Updated blueprint objects and registered routes

--- flask-app/src/__init__.py
+++ flask-app/src/__init__.py
@@ -19,7 +19,7 @@ def create_app():
     app.config['MYSQL_DATABASE_PASSWORD'] = open('/secrets/db_root_password.txt').readline().strip()
     app.config['MYSQL_DATABASE_HOST'] = 'db'
     app.config['MYSQL_DATABASE_PORT'] = 3306
-    app.config['MYSQL_DATABASE_DB'] = 'northwind'  # Change this to your DB name
+    app.config['MYSQL_DATABASE_DB'] = 'spotigram'  # Change this to your DB name
 
     # Initialize the database object with the settings above. 
     db.init_app(app)
@@ -32,14 +32,24 @@ def create_app():
     def welcome():
         return "<h1>Welcome to the 3200 boilerplate app</h1>"
 
-    # Import the various Beluprint Objects
+    # Import the various Blueprint Objects
     from src.users.users import users
     from src.artists.artists  import artists
+    from src.community.community import community
+    from src.curator.curators import curators
+    from src.producers.producers import producers
+    from src.playlist.playlists import playlists
+    from src.userDownload.userdownload import downloads
 
     # Register the routes from each Blueprint with the app object
     # and give a url prefix to each
     app.register_blueprint(users,   url_prefix='/u')
     app.register_blueprint(artists,    url_prefix='/a')
+    app.register_blueprint(community,   url_prefix='/cm/')
+    app.register_blueprint(curators,    url_prefix='/c')
+    app.register_blueprint(producers,    url_prefix='/p')
+    app.register_blueprint(playlists,    url_prefix='/pl')
+    app.register_blueprint(downloads,    url_prefix='/d')
 
     # Don't forget to return the app object
     return app
